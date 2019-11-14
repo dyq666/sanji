4f88036bdc21ac3490ca644e7ddd6168a8c8fe92
dyq666 <694526892@qq.com>
2019-11-14 14:01:10 +0800

    feature: add `OAuth2` and supply doc for `requests` util

--- util_requests.py
+++ util_requests.py
@@ -1,40 +1,67 @@
 __all__ = (
+    'OAuth2',
     'SessionWithUrlPrefix',
     'upload',
 )
 
 import os
-from typing import IO, Optional, Union
+from typing import TYPE_CHECKING, Optional, Union
 
-from requests import Session, Response
+from requests.auth import AuthBase
+from requests.sessions import Session
+
+if TYPE_CHECKING:
+    import io
+    from requests import Request, Response
 
 
 class SessionWithUrlPrefix(Session):
+    """可设置前缀的 session.
 
-    def __init__(self, url_prefix: Optional[str] = None):
+    >>> s = SessionWithUrlPrefix('http://localhost/api')
+    >>> r = s.get('/user')  # r.request.url == 'http://localhost/api/user'
+    """
+
+    def __init__(self, prefix: Optional[str] = None):
         super().__init__()
-        self.url_prefix = url_prefix
+        self.prefix = prefix
 
     def __repr__(self):
         return (
             f'<{self.__class__.__name__}'
-            f' url_prefix={self.url_prefix!r}'
+            f' prefix={self.prefix!r}'
             f'>'
         )
 
-    def request(self, method: str, url: str, *args, **kwargs) -> Response:
-        url = self.url_prefix + url if self.url_prefix is not None else url
+    def request(self, method: str, url: str, *args, **kwargs) -> 'Response':
+        url = url if self.prefix is None else self.prefix + url
         return super().request(method, url, *args, **kwargs)
 
 
-def upload(url: str, file: Union[str, IO],
-           file_name: str = None) -> Response:
-    """上传文件
+class OAuth2(AuthBase):
+    """自定义 OAuth2.
 
-    file: 可以是一个 str 代表文件路径, 也可以是一个类文件对象, 比如 io.StringIO.
+    >>> s = Session()
+    >>> s.auth = OAuth2('123')
+    >>> r = s.get('http://localhost/api/user')
+    """
 
-    file_name: 上传的文件名, 如果不指定并且 ``file`` 类型是 str 则从 ``file`` 中提取,
-               如果 ``file`` 类型不是 str, 则默认值为 data.csv
+    def __init__(self, token: str):
+        self.token = token
+
+    def __call__(self, r: 'Request') -> 'Request':
+        r.headers['Authorization'] = f'Bearer {self.token}'
+        return r
+
+
+def upload(url: str, file: Union[str, io.StringIO],
+           file_name: str = None) -> 'Response':
+    """上传文件到某个 url.
+
+    `file`: 可以是一个 str 代表文件路径, 也可以是一个类文件对象, 比如 `io.StringIO`.
+    `file_name`: 上传的文件名, 如果指定, 则为此参数.
+                 如果不指定此参数且 `file` 类型是 `str`, 那么从 `file` 中提取,
+                 如果不指定此参数且 `file` 类型是 `io.StringIO`, 那么为 `data.csv`.
     """
     import requests
 
