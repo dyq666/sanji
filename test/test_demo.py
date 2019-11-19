84f11ccdb626fe27b7d6cd99d092ada350938f3c
dyq666 <694526892@qq.com>
2019-11-19 17:05:06 +0800

    opt: use more test case for `CaseInsensitiveDict`

--- test/test_demo.py
+++ test/test_demo.py
@@ -1,20 +1,49 @@
+import string
+import pytest
+
 from util import CaseInsensitiveDict
 
 
-def test_CaseInsensitiveDict():
-    d = CaseInsensitiveDict()
-    content_type = 'application/json'
+class TestCaseInsensitiveDict:
+    """测试了下面的 11 个字典的方法
 
-    # __setitem__, __getitem__
-    d['Content-Type'] = content_type
-    assert d['content-type'] == content_type
+    ```
+    __getitem__  __setitem__  __delitem__
+    __len__  __iter__  __contains__
+    get items values keys fromkeys
+    ```
+    """
 
-    # get
-    assert d.get('content-Type') == content_type
-    assert d.get('dsa') is None
-    assert d.get('dasda', 1) == 1
+    @pytest.fixture
+    def d(self):
+        return CaseInsensitiveDict()
 
-    # __delitem__, __contains__
-    assert 'content-type' in d
-    del d['ContenT-Type']
-    assert 'content-type' not in d
+    def test_get_and_getitem(self, d):
+        d['c'] = 2
+        assert d.get('c') == 2
+        assert d.get('C') == 2
+        assert d.get('d') is None
+        assert d.get('D', 1) == 1
+        assert d['c'] == 2
+        assert d['C'] == 2
+
+    def test_setitem_and_len_and_items(self, d):
+        d['c'] = 2
+        assert dict(d.items()) == {'c': 2}
+        assert len(d) == 1
+        d['C'] = 2
+        assert dict(d.items()) == {'c': 2}
+        assert len(d) == 1
+
+    def test_del_and_contains(self, d):
+        d['c'] = 2
+        assert 'C' in d
+        del d['C']
+        assert 'C' not in d
+        assert None not in d
+
+    def test_fromkeys_and_keys_and_values_and_iter(self):
+        d = CaseInsensitiveDict.fromkeys(string.ascii_lowercase + string.ascii_uppercase, '1')
+        assert ''.join(d.keys()) == string.ascii_lowercase
+        assert ''.join(d) == string.ascii_lowercase
+        assert ''.join(d.values()) == '1' * len(d)
