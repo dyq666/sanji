bbf57a243316774ec717949bd7c86447d0e78219
dyq666 <694526892@qq.com>
2019-12-10 19:15:27 +0800

    feature: add util `get_create_query`

--- util/third_peewee.py
+++ util/third_peewee.py
@@ -1,15 +1,21 @@
 __all__ = (
+    'get_create_query',
     'query_2_sql',
 )
 
 from typing import TYPE_CHECKING, Tuple
 
 if TYPE_CHECKING:
-    from peewee import ModelSelect, Database
+    from peewee import Model, ModelSelect, Database
+
+
+def get_create_query(model: 'Model') -> 'ModelSelect':
+    """返回创建 model 的 `query` 对象."""
+    return model._schema._create_table()
 
 
 def query_2_sql(query: 'ModelSelect', db: 'Database') -> Tuple[str, str]:
-    """将 query 对象转为 sql 字符串."""
+    """将 `query` 对象转为 sql 字符串."""
     ctx = db.get_sql_context()
     sql, params = ctx.sql(query).query()
     return sql, params
