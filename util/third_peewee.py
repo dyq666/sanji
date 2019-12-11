cedc538beeef1c78ef41b1270485ffd8418e1239
dyq666 <694526892@qq.com>
2019-12-11 20:56:42 +0800

    opt: rename `query_2_sql` -> `query2sql`

--- util/third_peewee.py
+++ util/third_peewee.py
@@ -1,6 +1,6 @@
 __all__ = (
     'get_create_query',
-    'query_2_sql',
+    'query2sql',
 )
 
 from typing import TYPE_CHECKING, Tuple
@@ -14,7 +14,7 @@ def get_create_query(model: 'Model') -> 'ModelSelect':
     return model._schema._create_table()
 
 
-def query_2_sql(query: 'ModelSelect', db: 'Database') -> Tuple[str, str]:
+def query2sql(query: 'ModelSelect', db: 'Database') -> Tuple[str, str]:
     """将 `query` 对象转为 sql 字符串."""
     ctx = db.get_sql_context()
     sql, params = ctx.sql(query).query()
