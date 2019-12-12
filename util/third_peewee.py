8379feafdeb3d34174b0dbc4b0288d88f7f953ee
dyq666 <694526892@qq.com>
2019-12-12 16:07:48 +0800

    opt: show single quote in `query2sql`

--- util/third_peewee.py
+++ util/third_peewee.py
@@ -3,7 +3,7 @@ __all__ = (
     'query2sql',
 )
 
-from typing import TYPE_CHECKING, Tuple
+from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
     from peewee import Model, ModelSelect, Database
@@ -14,8 +14,10 @@ def get_create_query(model: 'Model') -> 'ModelSelect':
     return model._schema._create_table()
 
 
-def query2sql(query: 'ModelSelect', db: 'Database') -> Tuple[str, str]:
+def query2sql(query: 'ModelSelect', db: 'Database') -> str:
     """将 `query` 对象转为 sql 字符串."""
     ctx = db.get_sql_context()
     sql, params = ctx.sql(query).query()
-    return sql, params
+    params = tuple(f"'{p}'" for p in params)
+    params = params[0] if len(params) == 1 else params
+    return sql % params
