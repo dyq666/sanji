41744d8a3435fad7250c156addd3b93e740a820e
dyq666 <694526892@qq.com>
2020-02-11 19:16:42 +0800

    opt: use `opertor.xx` replace few relational operators

--- util/third_dateutil.py
+++ util/third_dateutil.py
@@ -2,23 +2,21 @@ __all__ = (
     'date_range',
 )
 
+import operator
 from datetime import date, datetime, timedelta
 from typing import Iterable, Union
 
 from dateutil.relativedelta import relativedelta
 
 Date = Union[date, datetime]
-TimeDelta = Union[relativedelta, timedelta]
 
 
-def date_range(start: Date, stop: Date, step: TimeDelta = timedelta(days=1),
+def date_range(start: Date,
+               stop: Date,
+               step: Union[relativedelta, timedelta] = timedelta(days=1),
                asc: bool = True) -> Iterable[Date]:
     """以 1 天为间隔迭代时间, 不包含 `end` (和内置的 `range` 函数保持一致)."""
-    if asc:
-        while start < stop:
-            yield start
-            start += step
-    else:
-        while start > stop:
-            yield start
-            start += step
+    o = operator.lt if asc else operator.gt
+    while o(start, stop):
+        yield start
+        start += step
