c49a1f7fed1699d2d8c0d4bd843e84a3a2896377
dyq666 <694526892@qq.com>
2020-02-12 10:17:32 +0800

    opt: update the doc of `date_range`

--- util/third_dateutil.py
+++ util/third_dateutil.py
@@ -15,7 +15,11 @@ def date_range(start: Date,
                stop: Date,
                step: Union[relativedelta, timedelta] = timedelta(days=1),
                asc: bool = True) -> Iterable[Date]:
-    """以 1 天为间隔迭代时间, 不包含 `end` (和内置的 `range` 函数保持一致)."""
+    """时间迭代器.
+
+    参数的命名和功能都与内置的 `range` 保持一致.
+    比较特殊的是增加了一个参数 `asc`, 用于标记是否为递增时间序列.
+    """
     o = operator.lt if asc else operator.gt
     while o(start, stop):
         yield start
